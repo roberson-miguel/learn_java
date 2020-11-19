445c437fab693f0bc93c27c9662bd813a0988532
Roberson Miguel <roberson.miguel@gmail.com>
2020-11-19 09:38:10 -0300

    Refatora o robo.
    Cria novo metodo.
    
    Melhora a responsabilidade dos metodos.

--- robo1.java
+++ robo1.java
@@ -1,4 +1,4 @@
-public class Robo {
+public class Robo2 {
     String nome;
     String cor;
     float velocidadeMax;
@@ -7,22 +7,36 @@ public class Robo {
     String tipoTracao;
     boolean temAntena;
     
+    public void printStatus(){
+        System.out.println("-------------------------------");
+        System.out.println("Meu nome: " + nome);
+        System.out.println("Cor do Robô: " + cor);
+        System.out.println("Velocidade Max: " + velocidadeMax);
+        System.out.println("Bateria: " + nivelBateriaAtual);
+        System.out.println("Carga Max: " + pesoCargaMax);
+        System.out.println("Tipo de Tração: " + tipoTracao);
+        System.out.println("Tem Antena: " + temAntena);
+        System.out.println("-------------------------------");
+    }
     public static void main(String[] args) {
-        Robo obj1 = new Robo();
-        obj1.nome = "R-801";
-        obj1.cor = "azul";
-        obj1.velocidadeMax = 6;
-        obj1.nivelBateriaAtual = 78;
-        obj1.pesoCargaMax = 10;
-        obj1.tipoTracao = "esteira";
-        obj1.temAntena = true;
+        Robo2 objeto1 = new Robo2();
+        objeto1.nome = "R-801";
+        objeto1.cor = "azul";
+        objeto1.velocidadeMax = 6;
+        objeto1.nivelBateriaAtual = 78;
+        objeto1.pesoCargaMax = 10;
+        objeto1.tipoTracao = "esteira";
+        objeto1.temAntena = true;
+        objeto1.printStatus();
         
-        System.out.println("Meu nome: " + obj1.nome );
-        System.out.println("Cor do Robô: " + obj1.cor);
-        System.out.println("Vel Max: " + obj1.velocidadeMax);
-        System.out.println("Bat: " + obj1.nivelBateriaAtual);
-        System.out.println("Carga Max: " + obj1.pesoCargaMax);
-        System.out.println("Tipo Tração: " + obj1.tipoTracao);
-        System.out.println("Tem Antena: " + obj1.temAntena);
+        Robo2 objeto2 = new Robo2();
+        objeto2.nome = "R-701";
+        objeto2.cor = "laranja";
+        objeto2.velocidadeMax = 3;
+        objeto2.nivelBateriaAtual = 51;
+        objeto2.pesoCargaMax = 15;
+        objeto2.tipoTracao = "esteira";
+        objeto2.temAntena = false;
+        objeto2.printStatus();
     }
 }
