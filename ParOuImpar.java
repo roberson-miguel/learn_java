96c9c68f5af17889ca0257158cb1b49063fead42
Robersob Miguel <roberson.miguel@gmail.com>
2024-07-29 18:31:21 -0300

    Garantido que as entradas sejam validas

--- ParOuImpar.java
+++ ParOuImpar.java
@@ -12,10 +12,27 @@ public class ParOuImpar {
         do {
             System.out.print("Escolha Par ou Ímpar (P/I): ");
             escolhaJogador = scanner.next().toUpperCase();
+            if (!escolhaJogador.equals("P") && !escolhaJogador.equals("I")) {
+                System.out.println("Entrada inválida. Por favor, escolha 'P' para Par ou 'I' para Ímpar.");
+            }
         } while (!escolhaJogador.equals("P") && !escolhaJogador.equals("I"));
         
-        System.out.print("Escolha um número de 0 a 5: ");
-        int numeroJogador = scanner.nextInt();
+        int numeroJogador = -1;
+        boolean numeroValido = false;
+        do {
+            System.out.print("Escolha um número de 0 a 5: ");
+            if (scanner.hasNextInt()) {
+                numeroJogador = scanner.nextInt();
+                if (numeroJogador >= 0 && numeroJogador <= 5) {
+                    numeroValido = true;
+                } else {
+                    System.out.println("Número inválido. Por favor, escolha um número entre 0 e 5.");
+                }
+            } else {
+                System.out.println("Entrada inválida. Por favor, insira um número.");
+                scanner.next(); // Limpa a entrada inválida
+            }
+        } while (!numeroValido);
         
         System.out.println("Você escolheu: " + numeroJogador);
         System.out.println("Computador está escolhendo um número...");
