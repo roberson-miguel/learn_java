480d70c4dec3cd581674aacc3828e5cd9da1e168
Robersob Miguel <roberson.miguel@gmail.com>
2024-07-29 16:24:15 -0300

    Adicionando emoção com o sleep

--- ParOuImpar.java
+++ ParOuImpar.java
@@ -13,11 +13,32 @@ public class ParOuImpar {
         System.out.print("Escolha um número de 0 a 5: ");
         int numeroJogador = scanner.nextInt();
         
+        System.out.println("Você escolheu: " + numeroJogador);
+        System.out.println("Computador está escolhendo um número...");
+        
+        try {
+            Thread.sleep(2000); // Pausa de 2 segundos
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
         int numeroComputador = random.nextInt(6); // Número aleatório de 0 a 5
         System.out.println("O computador escolheu: " + numeroComputador);
         
+        try {
+            Thread.sleep(1500); // Pausa de 1.5 segundos
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
         int soma = numeroJogador + numeroComputador;
         System.out.println("A soma é: " + soma);
+        
+        try {
+            Thread.sleep(1500); // Pausa de 1.5 segundos
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         
         boolean jogadorVenceu = (soma % 2 == 0 && escolhaJogador.equals("P")) ||
                                 (soma % 2 != 0 && escolhaJogador.equals("I"));
@@ -31,3 +52,4 @@ public class ParOuImpar {
         scanner.close();
     }
 }
+
